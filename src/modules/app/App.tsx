01959741d48d00ef3d74b4051fa4ba5697a84c47
do0dleman <97869897+do0dleman@users.noreply.github.com>
2023-04-02 19:01:12 +0300

    fixing font issue

--- src/modules/app/App.tsx
+++ src/modules/app/App.tsx
@@ -4,6 +4,11 @@ import WeatherContextProvider from '../../components/WeatherContextProvider/Weat
 import Forecast from '../forecast/Forecast'
 import Header from '../header/Header'
 import './App.scss'
+import '../../fonts/wi-font/weathericons-regular-webfont.eot'
+import '../../fonts/wi-font/weathericons-regular-webfont.svg'
+import '../../fonts/wi-font/weathericons-regular-webfont.ttf'
+import '../../fonts/wi-font/weathericons-regular-webfont.woff'
+import '../../fonts/wi-font/weathericons-regular-webfont.woff2'
 
 function App() {
 
